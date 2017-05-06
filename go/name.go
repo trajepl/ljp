bef022e59547e443f18c5fee94d5955e34319ef5
trajepl <trajepl@gmail.com>
2017-05-06 19:21:33 +0800

    go stack

--- go/name.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-    var c int
-    for i := 0; i < 10; i++ {
-        c++
-    }
-    println(c)
-}
---
+++ go/stack.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "error"
+)
+
+func main() {
+    stack := make([]int, 0, 5)
+    push := func(x int) error {
+
+    }
+
+    pop := func() (int, error) {
+    }
+}
