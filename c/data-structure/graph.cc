b512a04c7cc707c9b3e19bf11c1c257973a09be8
trajep <trajepl@gmail.com>
2017-10-03 13:05:38 +0800

    datastruct-graph.traverse

--- c/data-structure/graph.cc
+++ c/data-structure/graph.cc
@@ -27,3 +27,44 @@ typedef struct Graph {
     int n, e;            // number of nodes and edges
 }Graph;
 
+// traverse graph
+// dfs-adjacency table
+int visit[maxSize];
+
+void dfs(Graph *G, int v) {
+    ArcNode *p;
+    visit[v] = 1;
+    Visit(v);           // operate the node V
+    p = G->adjlist[v].first;
+    while(p != NULL) {
+        if(visit[p->id] == 0) {
+            dfs(G, p->id);
+            p = p->next;
+        }
+    }
+}
+
+// bfs-adjacency
+void bfs(Graph *G, int v, int visit[maxSize]) {
+    ArcNode *p;
+    int que[maxSize], front = 0, rear = 0;
+    int j;
+    Visit(v);
+    visit[v] = 1;
+    rear = (rear + 1) % maxSize;
+    que[rear] = v;
+    while(front != rear) {
+        front = (front + 1) % maxSize;
+        j = que[front];
+        p = G->adjlist[j].first;
+        while(p != NULL) {
+            if(visit[p->id] == 0) {
+                Visit(p->id);
+                visit[p->id] = 1;
+                rear = (rear + 1) % maxSize;
+                que[rear] = p->id;
+            }
+            p = p->next;
+        }
+    }
+}
